6dd6f4df96631ca3df1b858432e7e438bfda7224
lehui99 <lehui99@07435f4b-e3e5-5c40-d676-e37e09da0046>
2011-03-25 09:43:38 +0000

    Fix deploy bug Add Started.

--- sh/run_with_polipo.bat
+++ sh/run_with_polipo.bat
@@ -2,6 +2,7 @@
 cd java
 if not exist phpsocks5.properties goto notdeploy
 start cmd /k polipo -c polipo.conf
+echo Started.
 java phpsocks5.PhpSocks5 > socks5out.log 2> socks5err.log
 goto runend
 :notdeploy
